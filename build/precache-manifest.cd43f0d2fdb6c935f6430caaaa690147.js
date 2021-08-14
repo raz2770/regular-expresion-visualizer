56e8cf826454ab36c71b04a0d50970d17f7abc9b
rajendra kumar <rajendrakumar2770@gmail.com>
2021-08-14 12:49:58 +0530

    netlify deploy done

---
+++ build/precache-manifest.cd43f0d2fdb6c935f6430caaaa690147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18e2a65153f4c9d3d83970c934732be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "182d4f5a158d78193c97",
+    "url": "/static/css/main.17bea3a9.chunk.css"
+  },
+  {
+    "revision": "af07e3fba53a52880b87",
+    "url": "/static/js/2.45bcaf39.chunk.js"
+  },
+  {
+    "revision": "daf30f9b8a50a959901051579f192eab",
+    "url": "/static/js/2.45bcaf39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "182d4f5a158d78193c97",
+    "url": "/static/js/main.17a2e3d2.chunk.js"
+  },
+  {
+    "revision": "c04af0c072eb45e79baf",
+    "url": "/static/js/runtime-main.521f289a.js"
+  }
+]);
